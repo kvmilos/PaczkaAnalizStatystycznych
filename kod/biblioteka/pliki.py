e28792f534555f3ef62e365918d36b4a13403e76
kamil <kamiltomaszek@icloud.com>
2023-01-21 00:01:41 +0100

    usunieto niepotrzebne pliki i zmieniono pliki.py

--- kod/biblioteka/pliki.py
+++ kod/biblioteka/pliki.py
@@ -1,3 +1,14 @@
 import pandas as pd
-def pobierz(sciezka1, sciezka2, sciezka3):
-    return pd.read_csv(sciezka1, skiprows = 4), pd.read_csv(sciezka2, skiprows = 4), pd.read_csv(sciezka3)
+import argparse as ap
+
+
+def pobierz_dane():
+    parser = ap.ArgumentParser()
+    parser.add_argument('GDP', help='GDP_file to be read')
+    parser.add_argument('Population', help='Population_file to be read')
+    parser.add_argument('Emissions', help='Emissions_file to be read')
+    parser.add_argument('rangemin', default=0, help='First year in range')
+    parser.add_argument('rangemax', default=0, help='Last year in range')
+    args = parser.parse_args()
+    return pd.read_csv(args.GDP, skiprows=4), pd.read_csv(args.Population, skiprows=4), pd.read_csv(
+        args.Emissions), args.rangemin, args.rangemax
