124e8362c8a754fb6254dae0cff669d61e34871e
kamil <kamiltomaszek@icloud.com>
2023-01-20 23:52:59 +0100

    Kolejny etap wykonany

---
+++ kod/biblioteka/analizy.py
@@ -0,0 +1,20 @@
+def max_co2_per_capita(dane):
+    return dane.groupby('Year').apply(lambda df: df.nlargest(5, 'Per Capita')[['Country', 'Per Capita', 'Total']])
+
+
+def max_gdp_per_capita(dane):
+    dane['GDP per capita'] = dane['GDP'] / dane['Population']
+    return dane.groupby('Year').apply(lambda df: df.nlargest(5, 'GDP per capita'))[['Country', 'GDP per capita', 'GDP']]
+
+
+def co2_change_per_capita(dane):
+    dane = dane.sort_values(by='Year')
+    last_year = dane.Year.unique()[-1]
+    year2 = last_year - 10
+    pivot = dane[dane.Year.isin([year2, last_year])].pivot_table(values='Per Capita', index='Country', columns='Year')
+    pivot['change'] = pivot[last_year] - pivot[year2]
+    pivot = pivot.dropna()
+    pivot = pivot.sort_values(by='change', ascending=False)
+    pivot2 = pivot.sort_values(by='change', ascending=True)
+    return pivot.head(5), pivot2.head(5)
+
---
+++ kod/operacje/nw.py
@@ -0,0 +1,19 @@
+from kod.biblioteka.pliki import pobierz_dane
+from kod.biblioteka.dane import przerob, oczysc, scal
+from kod.biblioteka.analizy import max_co2_per_capita, max_gdp_per_capita, co2_change_per_capita
+
+
+def main():
+    gdp, pop, co2, rangemin, rangemax = pobierz_dane()
+    gdp, pop, co2 = przerob(gdp, pop, co2, rangemin, rangemax)
+    gdp, pop, co2 = oczysc(gdp, pop, co2)
+    dane = scal(gdp, pop, co2)
+    print(dane)
+    print(max_co2_per_capita(dane))
+    print(max_gdp_per_capita(dane))
+    gora, dol = co2_change_per_capita(dane)
+    print(gora)
+    print(dol)
+
+
+main()
