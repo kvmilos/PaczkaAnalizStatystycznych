5eb85ba75b938460caeda23932782939b9e99368
kamil <kamiltomaszek@icloud.com>
2023-01-21 01:23:14 +0100

    testowanie

--- kod/biblioteka/pliki.py
+++ kod/biblioteka/pliki.py
@@ -7,8 +7,16 @@ def pobierz_dane():
     parser.add_argument('GDP', help='GDP_file to be read')
     parser.add_argument('Population', help='Population_file to be read')
     parser.add_argument('Emissions', help='Emissions_file to be read')
-    parser.add_argument('rangemin', default=0, help='First year in range')
-    parser.add_argument('rangemax', default=0, help='Last year in range')
+    parser.add_argument('rangemin', default=0, help='First year in range', nargs="?")
+    parser.add_argument('rangemax', default=3000, help='Last year in range', nargs="?")
     args = parser.parse_args()
+    try:
+        assert isinstance(int(args.rangemin), int)
+    except ValueError:
+        raise KeyError("The first year in range must be a number!")
+    try:
+        assert isinstance(int(args.rangemax), int)
+    except ValueError:
+        raise KeyError("The last year in range must be a number!")
     return pd.read_csv(args.GDP, skiprows=4), pd.read_csv(args.Population, skiprows=4), pd.read_csv(
-        args.Emissions), args.rangemin, args.rangemax
+        args.Emissions), int(args.rangemin), int(args.rangemax)
